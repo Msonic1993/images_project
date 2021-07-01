2286ac7877e472e25acdd1d8e0b8026fa4eb7523
Wojciech Dziwoki <wojciech.dziwoki@gmail.com>
2021-07-01 19:52:30 +0200

    Commit to test release heroku

--- project/settings.py
+++ project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-hhmlteloyys14&-z!%^g12#lp#xxa7or_!mdj5nd&!s1fl-wfy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*','images-api-task.herokuapp.com']
 
 
 # Application definition
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 ]
 SITE_ID = 1
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -147,3 +148,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#  Add configuration for static files storage using whitenoise
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'

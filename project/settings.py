65333c408a851d9a6ae060d61e5f67d106c87361
Wojciech Dziwoki <wojciech.dziwoki@gmail.com>
2021-07-01 19:19:14 +0200

    Commit to test release

--- project/settings.py
+++ project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hhmlteloyys14&-z!%^g12#lp#xxa7or_!mdj5nd&!s1fl-wfy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
